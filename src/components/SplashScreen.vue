1fb414b567321ac89d954aeb91e2be7f8e57ba70
Omar Terrazas <terrazas.omar@gmail.com>
2023-04-20 16:08:55 -0600

    Splash screen has been integrated

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="splashscreen">
+    <img src="@/assets/images/cashflow-icon.svg" alt="cashflow" />
+    <h1><span>Cash</span>Flow</h1>
+  </div>
+</template>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
