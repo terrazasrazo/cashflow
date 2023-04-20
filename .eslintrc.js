792fe42be116acfc4c458ac86e5a70da3feedb7a
Omar Terrazas <terrazas.omar@gmail.com>
2023-04-20 15:42:31 -0600

    Override ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,4 +15,12 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
+  overrides: [
+    {
+      files: ["src/components/**/*.vue"],
+      rules: {
+        "vue/multi-word-component-names": 0,
+      },
+    },
+  ],
 };
